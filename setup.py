b2a25eeac5db899545fc51a1610cafb76ef3633e
therealssj <mehul.guptagm@gmail.com>
2017-12-12 02:40:27 +0530

    Setup click

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='opengapps-gsfcreator',
+      version='0.0.1',
+      description='CLI to make the task of creating gsf ids easier',
+      author='Mehul Gupta(therealssj)',
+      author_email='mehul.guptagm@gmail.com',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'gsfcreator = gsfcreator:cli',
+          ]
+      },
+      url='https://github.com/therealssj/opengapps-gsfcreator',
+      keywords=['gsfcreator', 'cli', 'command-line', 'python', 'opengapps'],
+      license='MIT',
+      classifiers=[],
+      install_requires=[
+            'click'
+      ]
+)
